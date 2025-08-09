beafc52f6de889691a3d78d8c5f3282976a3f7f0
Ethan Pervere <ethan.pervere@gmail.com>
2025-08-08 21:57:46 -0400

    Update graph-events.js

--- js/graph-events.js
+++ js/graph-events.js
@@ -150,6 +150,13 @@ document.addEventListener('mousedown', (event) => {
         const modelRect = model.getBoundingClientRect();
         state.currentVertex.style.left = (event.clientX - modelRect.left - 24) + 'px';
         state.currentVertex.style.top = (event.clientY - modelRect.top - 24) + 'px';
+
+        // Generated
+        state.edges.forEach(({ v1, v2, update }) => {
+            if (v1 === state.currentVertex || v2 === state.currentVertex) {
+                update();
+            }
+        });
     }
 });
 
